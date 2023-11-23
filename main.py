7f81e12ceca3bc42011b27ef1557e390c2cf1433
Pierre <pdepretz59@gmail.com>
2023-11-23 11:40:53 +1100

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from bson.binary import Binary
 
 
 class PyHack(object):
-    def __init__(self, dbPath):
-        conn = wiredtiger_open(dbPath, 'create,cache_size=512M,session_max=33000,eviction=(threads_min=4,threads_max=4),config_base=false,statistics=(fast),log=(enabled=true,archive=true,path=journal,compressor=snappy),file_manager=(close_idle_time=100000,close_scan_interval=10,close_handle_minimum=250),statistics_log=(wait=0),verbose=(),compatibility=(release="3.3", require_min="3.2.0")')
-        self.dbPath = dbPath
+    def __init__(self, db_path):
+        conn = wiredtiger_open(db_path, 'create,cache_size=512M,session_max=33000,eviction=(threads_min=4,threads_max=4),config_base=false,statistics=(fast),log=(enabled=true,archive=true,path=journal,compressor=snappy),file_manager=(close_idle_time=100000,close_scan_interval=10,close_handle_minimum=250),statistics_log=(wait=0),verbose=(version),compatibility=(release="3.3", require_min="3.2.0")')
+        self.db_path = db_path
 
         #MongoDB
         self.database = "_" + self.__class__.__name__
@@ -17,18 +17,21 @@ class PyHack(object):
         self.values = []
 
         #WiredTiger
-        self.mdbCatalog = '_mdb_catalog'
+        self.mdb_catalog = '_mdb_catalog'
 
         self.__session = conn.open_session()
         print("Connection opened")
 
     def checkpoint_session(self):
+        """Function to Checkpoint a session"""
         return self.__session.checkpoint()
 
     def get_new_cursor(self, table):
+        """Function to create a new cursor"""
         return self.__session.open_cursor(f'table:{table}', None, "append")
     
     def get_k_v(self, table):
+        """Function to return keys and values in a table"""
         k_v = {}
         cursor = self.get_new_cursor(table)
 
@@ -40,49 +43,53 @@ class PyHack(object):
         return k_v
     
     def get_new_k(self, table):
-        newKey = 0
+        """Function to generate a new key in a table"""
+        new_key = 0
         for key in self.get_k_v(table):
-            newKey = int(key) + 1
+            new_key = int(key) + 1
 
-        return newKey
+        return new_key
     
     def get_ident(self, namespace):
-        catalog = self.get_k_v(self.mdbCatalog)
+        """Function to get ident of a file"""
+        catalog = self.get_k_v(self.mdb_catalog)
 
         for k,v in catalog.items():
             if 'md' in v:
                 if(v['md']['ns'] == namespace):
                     return v['ident']
-        else:
-            return None
+
+        return None
 
 
     def get_stats(self, table=None):
+        """Function to get stats of a table"""
         if not table:
-            cursor = self.__session.open_cursor(f'statistics:', None)
+            cursor = self.__session.open_cursor("statistics:", None)
         else:
-            cursor = self.__session.open_cursor(f'statistics:table:{table}', None)
+            cursor = self.__session.open_cursor(f"statistics:table:{table}", None)
 
         stats = []
         while cursor.next() == 0:
             stats.append(cursor.get_value())
         
         return stats
-
-    def get_catalog(self):
-        return self.mdbCatalog
     
     def create_table(self, table):
+        """Function to create a new table"""
         self.__session.create(f"table:{table}", "key_format=q,value_format=u,type=file,memory_page_max=10m,split_pct=90,leaf_value_max=64MB,checksum=on,block_compressor=snappy,app_metadata=(formatVersion=1),log=(enabled=false)")
 
     def drop_table(self, table):
+        """Function to drop a table"""
         self.__session.drop(f"table:{table}")
 
     def close_session(self):
+        """Function to close a WT session"""
         print("Connection closed")
         return self.__session.close()
 
     def get_values(self, table, key=None):
+        """Function to get all values in a Table"""
         if key is not None:
             cursor = self.get_new_cursor(table)
             result = bson.decode(cursor[key])
@@ -96,6 +103,7 @@ class PyHack(object):
 
     #Ex for records: {"id1":{ "key1":"value1" }, "id2": { "key2":"value2" }, ...}
     def insert_records(self, table, records):
+        """Function to insert a list of records in a Table"""
         for key in records.keys():
             value = bson.encode(records[key])
 
@@ -108,6 +116,7 @@ class PyHack(object):
 
 
     def delete_record(self, table, key):
+        """Function to delete one record in a table"""
         cursor = self.get_new_cursor(table)
 
         cursor.set_key(key)
@@ -118,10 +127,11 @@ class PyHack(object):
 
 
 def main():
+    """Run function"""
     print(WIREDTIGER_VERSION_STRING)
 
     wt = PyHack("data/db")
-    wtCatalogName = wt.get_catalog()
+    wt_catalog = wt.mdb_catalog
     namespace = None
 
     #collect arguments
@@ -148,21 +158,21 @@ def main():
         ident = wt.get_ident(namespace)
         
         if ident:
-            collDocuments = wt.get_values(ident)
-            if collDocuments:
-                print(f"Documents in the collection ({wt.collection}): {collDocuments}")
+            coll_documents = wt.get_values(ident)
+            if coll_documents:
+                print(f"Documents in the collection ({wt.collection}): {coll_documents}")
 
             print("Do you wanna drop this collection ? (y/n)")
             drop = input().lower()
 
             if drop == "y":
-                catalog = wt.get_k_v(wtCatalogName)
+                catalog = wt.get_k_v(wt_catalog)
 
                 for k, v in catalog.items():
                     if 'md' in v:
                         if v['ident'] == ident:
                             wt.delete_record(
-                                wt.mdbCatalog, 
+                                wt.mdb_catalog, 
                                 k
                             )
 
@@ -172,37 +182,37 @@ def main():
             ident = wt.collection
             wt.create_table(ident)
             print(f"-- Collection {wt.collection} created")
-            newKey = wt.get_new_k(wt.mdbCatalog)
+            new_key = wt.get_new_k(wt.mdb_catalog)
             uuid_binary = Binary(uuid.uuid4().bytes, 4)
             
             #new entry in catalogue
             wt.insert_records(
-                wt.mdbCatalog, 
-                { 
-                    newKey:
+                wt.mdb_catalog,
+                {
+                    new_key:
                     {
                         'md': {
                             'ns': namespace, 
                             'options': {
                                 'uuid': uuid_binary
                                 }
-                            }, 
+                            },
                             'ns': namespace, 
                             'ident': ident
                     }
                 }
             )
-            print(f"-- Catalog updated with the new collection")
+            print("-- Catalog updated with the new collection")
 
         #Check if value is set
         if wt.values:
             print("Insert value(s):")
             for value in wt.values:   
-                newKey = wt.get_new_k(ident)
+                new_key = wt.get_new_k(ident)
                 wt.insert_records(
-                    ident, 
+                    ident,
                     {
-                        newKey: 
+                        new_key:
                         {
                             'field': value
                         }
@@ -211,7 +221,7 @@ def main():
 
     #Drop all user collections
     else:
-        catalog = wt.get_k_v(wtCatalogName)
+        catalog = wt.get_k_v(wt_catalog)
         print("List all collections:")
 
         for k,v in catalog.items():
